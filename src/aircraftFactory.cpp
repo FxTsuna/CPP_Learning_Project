cae94a3380bbe5bc4afabf4db3a91bec5340950a
FxTsuna <FxTsuna@outlook.fr>
2022-04-14 01:37:33 +0200

    Task 4 q6

--- src/aircraftFactory.cpp
+++ src/aircraftFactory.cpp
@@ -5,7 +5,7 @@ std::unique_ptr<Aircraft> aircraftFactory::create_aircraft(const AircraftType& t
     assert(airport); // make sure the airport is initialized before creating aircraft
     const std::string flight_number = airlines[std::rand() % 8] + std::to_string(1000 + (rand() % 9000));
     const float angle       = (rand() % 1000) * 2 * 3.141592f / 1000.f; // random angle between 0 and 2pi
-    const Point3D start     = Point3D { std::sin(angle), std::cos(angle), 0 } * 3 + Point3D { 0, 0, 2 };
+    const Point3D start     = Point3D { std::sin(angle), std::cos(angle), 0.f } * 3 + Point3D { 0.f, 0.f, 2.f };
     const Point3D direction = (-start).normalize();
 
     return std::make_unique<Aircraft>(type, flight_number, start, direction, airport->get_tower());
--- src/geometry.hpp
+++ src/geometry.hpp
@@ -11,14 +11,29 @@ template <const int size, typename t>
 
 struct Point
 {
-    using type = Point<size, t>;
     std::array<t, size> values {};
     Point() {}
+
+
+    template <typename... Args>
+    Point(t first, Args&&... args) : values {first, std::forward<Args>(args)...}
+    { static_assert(sizeof ...(args) + 1 == size, "Not corresponding arguments");}
+    /*
     Point(t x, t y) : values { x, y }
     { static_assert(size == 2,"Not a Point2D"); }
     Point(t x, t y, t z) : values { x, y, z }
     { static_assert(size == 3,"Not a Point3D"); }
+     */
 
+
+    /*friend void test_generic_points()
+    {
+        Point<3,int> p1;
+        Point<3,int> p2;
+        auto p3 = p1 + p2;
+        p1 += p2;
+        p1 *= 3; // ou 3.f, ou 3.0 en fonction du type de Point
+    }*/
 
     float& x()
     {
@@ -53,60 +68,60 @@ struct Point
         return values[2];
     }
 
-    type& operator+=(const type& other)
+    Point& operator+=(const Point& other)
     {
         std::transform(values.begin(),values.end(),other.values.begin(),values.begin(),std::plus<float> {});
         return *this;
     }
 
-    type& operator-=(const type& other)
+    Point& operator-=(const Point& other)
     {
         std::transform(values.begin(),values.end(),other.values.begin(),values.begin(),std::minus<float> {});
         return *this;
     }
 
-    type& operator*=(const float scalar)
+    Point& operator*=(const float scalar)
     {
         std::transform(values.begin(),values.end(),values.begin(),[scalar](float c) { return c*scalar; });
         return *this;
     }
 
-    type operator+(const type& other) const
+    Point operator+(const Point& other) const
     {
-        type result = *this;
+        Point result = *this;
         result += other;
         return result;
     }
 
-    type operator-(const type& other) const
+    Point operator-(const Point& other) const
     {
-        type result = *this;
+        Point result = *this;
         result -= other;
         return result;
     }
 
-    type operator*(const t scalar) const
+    Point operator*(const t scalar) const
     {
-        type result = *this;
+        Point result = *this;
         result *= scalar;
         return result;
     }
 
-    type operator-() const
+    Point operator-() const
     {
-        return type { -x(), -y(), -z() };
+        return Point { -x(), -y(), -z() };
     }
 
     float length() const {
         return std::sqrt(std::inner_product(values.begin(), values.end(), values.begin(), float { 0 }));
     }
 
-    float distance_to(const type& other) const
+    float distance_to(const Point& other) const
     {
         return (*this - other).length();
     }
 
-    type& normalize(const float target_len = 1.0f)
+    Point& normalize(const float target_len = 1.0f)
     {
         const float current_len = length();
         if (current_len == 0)
@@ -118,7 +133,7 @@ struct Point
         return *this;
     }
 
-    type& cap_length(const float max_len)
+    Point& cap_length(const float max_len)
     {
         assert(max_len > 0);
 
@@ -131,14 +146,14 @@ struct Point
         return *this;
     }
 
-    type operator*(const type& other) const
+    Point operator*(const Point& other) const
     {
-        type result = *this;
+        Point result = *this;
         result *= other;
         return result;
     }
 
-    type& operator*=(const type& other)
+    Point& operator*=(const Point& other)
     {
         x() *= other.x();
         y() *= other.y();
